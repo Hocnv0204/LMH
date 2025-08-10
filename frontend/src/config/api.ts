d32d12e58d5036b60bfd9e839e9d26f703878665
PhQuangMinh <phamquangminh15012004@gmail.com>
2025-08-10 23:50:22 +0700

    feat: ghep fe + be deploy

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -1,7 +1,7 @@
 // API Configuration
 export const API_CONFIG = {
   // BASE_URL: 'http://localhost:8081',
-  BASE_URL: 'http://13.211.99.103:8090',
+  BASE_URL: 'https://13.211.99.103:8090',
 } as const;
 
 // Helper function to build API URLs
